8bba99134cc63f913a5f12ad4a413891bf4c89e3
Garbakbl <121131132+Garbakbl@users.noreply.github.com>
2023-04-05 16:50:02 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -4,9 +4,6 @@ from scipy.optimize import minimize
 chat_id = 356550601
 
 def solution(x: np.array) -> float:
-    def negative_log_likelihood(params, x):
-        alpha, sigma_sq = params
-        return -np.sum(251 + np.log(1 / (x * np.sqrt(2 * np.pi * sigma_sq))) - ((np.log(x) - alpha) ** 2) / (2 * sigma_sq))
-
-    res = minimize(negative_log_likelihood, (0, 1), args=x, method='Nelder-Mead')
-    return res.x[0]
+    log_data = np.log(x-251)
+    mu = np.mean(log_data)
+    return mu
